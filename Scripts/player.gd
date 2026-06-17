dc7827e66123bf3a86a2e0335d325a57b35b3987
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2026-06-17 17:14:57 +0300

    Update player, Update main scene, Add globals

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 # Horizontal Movement
 const MOVE_SPEED: int = 500
-const START_MOVE_DELTA: float = 0.25
+const START_MOVE_DELTA: float = 0.2
 const GROUND_STOP_DELTA: float = 0.2
 const AIR_STOP_DELTA: float = 0.1
 const BUMP_STOP_DELTA: float = 0.3
@@ -13,14 +13,14 @@ var bumping: bool
 
 # Jump
 const JUMP_POWER: int = 750
-const NORMAL_GRAVITY: int = 2000
-const FAST_FALL_GRAVITY: int = 2500
+const NORMAL_GRAVITY: int = 2500
+const FAST_FALL_GRAVITY: int = 3000
 const COTOYE_TIME_SECONDS: float = 0.1
 const JUMP_BUFFER_SECONDS: float = 0.2
 var coyote_time_counter: float
 var jump_buffer_counter: float
-var double_jumped: bool = false
 var grounded: bool
+var double_jumped: bool
 
 func _process(_delta):
 	move_direction = Input.get_axis("move_left", "move_right") # Get horizontal inputs
@@ -93,7 +93,7 @@ func _physics_process(delta: float) -> void:
 	if jump_buffer_counter > 0 && coyote_time_counter > 0: # Normal jump
 		# TODO: Play the normal jump sound here
 		jump()
-	elif !double_jumped && jump_buffer_counter > 0 && !(coyote_time_counter > 0): # Double jump
+	elif Globals.can_double_jump && !double_jumped && jump_buffer_counter > 0 && !(coyote_time_counter > 0): # Double jump
 		# TODO: Play the double jump sound here
 		double_jumped = true
 		jump()
