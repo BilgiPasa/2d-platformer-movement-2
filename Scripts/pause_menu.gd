3b4e8c6ae9ee8f9eb94e5f1aee83c6052b8b810d
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2026-08-05 21:31:17 +0300

    Add info label to PauseMenu about player controls, Update PauseMenu scene and script, Update to version 1.3.4

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -5,9 +5,9 @@ signal show_speed_lbl
 signal hide_speed_lbl
 
 @export var resume_button: Button
-@export var hide_speed_button: Button
-@export var enable_d_jump_btn: Button
-@export var mute_sounds_btn: Button
+@export var speed_label_on_off_button: Button
+@export var d_jump_on_off_button: Button
+@export var sounds_on_off_button: Button
 @export var version_label: Label
 
 func _ready() -> void:
@@ -26,30 +26,30 @@ func _on_quit_game_button_pressed() -> void:
 func quit_game() -> void:
 	get_tree().quit()
 
-func _on_hide_speed_button_pressed() -> void:
+func _on_spd_lbl_on_off_btn_pressed() -> void:
 	if Globals.speed_label_hidden: # Show
 		Globals.speed_label_hidden = false
 		show_speed_lbl.emit()
-		hide_speed_button.text = "Hide Speed"
+		speed_label_on_off_button.text = "Hide Speed Label"
 	else: # Hide
 		Globals.speed_label_hidden = true
 		hide_speed_lbl.emit()
-		hide_speed_button.text = "Show Speed"
+		speed_label_on_off_button.text = "Show Speed Label"
 
-func _on_enable_d_jump_btn_pressed() -> void:
+func _on_d_jump_on_off_btn_pressed() -> void:
 	if Globals.can_double_jump: # Disable
 		Globals.can_double_jump = false
-		enable_d_jump_btn.text = "Enable Double Jump"
+		d_jump_on_off_button.text = "Enable Double Jump"
 	else: # Enable
 		Globals.can_double_jump = true
-		enable_d_jump_btn.text = "Disable Double Jump"
+		d_jump_on_off_button.text = "Disable Double Jump"
 
-func _on_mute_sounds_btn_pressed() -> void:
+func _on_sounds_on_off_btn_pressed() -> void:
 	if Globals.sounds_muted: # Unmute
 		Globals.sounds_muted = false
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
-		mute_sounds_btn.text = "Mute Sounds"
+		sounds_on_off_button.text = "Mute Sounds"
 	else: # Mute
 		Globals.sounds_muted = true
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
-		mute_sounds_btn.text = "Unmute Sounds"
+		sounds_on_off_button.text = "Unmute Sounds"
